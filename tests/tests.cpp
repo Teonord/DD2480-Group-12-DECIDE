c4a1b822149d423bc1e74f118678443a46f5aac8
Teonord <teo.nordstrom@outlook.com>
2025-01-23 16:24:16 +0100

    feat: doubleCompare function from spec
    Added double comparison function from specification, that results equal if two numbers are similar enough.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,3 +2,20 @@
 
 #include "../external/catch.hpp"
 #include "../include/decide.hpp"
+
+TEST_CASE( "Rounded double comparison (doubleCompare)", "[doubleCompare]") {
+    REQUIRE( doubleCompare(0, 0) == EQ);
+    REQUIRE( doubleCompare(-10, -10) == EQ);
+    REQUIRE( doubleCompare(7, 7) == EQ);
+
+    REQUIRE( doubleCompare(-99999, 99999) == LT);
+    REQUIRE( doubleCompare(0, 1) == LT);
+    REQUIRE( doubleCompare(0, 0.000001) == LT);
+    
+    REQUIRE( doubleCompare(99999, -99999) == GT);
+    REQUIRE( doubleCompare(1, 0) == GT);
+    REQUIRE( doubleCompare(0.000001, 0) == GT);
+
+    REQUIRE( doubleCompare(0, 0.0000001) == EQ);
+    REQUIRE( doubleCompare(0.0000001, 0) == EQ);
+}
