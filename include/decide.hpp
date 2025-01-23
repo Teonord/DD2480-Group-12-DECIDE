1746992644e7fcf3c0861e4bd39d844ac35a7901
Teonord <teo.nordstrom@outlook.com>
2025-01-23 16:02:04 +0100

    init: add global variables
    Add global variables and declarations from the specifications in decide.pdf

---
+++ include/decide.hpp
@@ -0,0 +1,70 @@
+#ifndef DECIDE_H
+#define DECIDE_H
+
+#include <cmath>
+
+static const double PI = 3.415926535;
+
+// Enum to know operator in LCM
+typedef enum { NOTUSED=777, ORR, ANDD } Connectors;
+
+// Enum to compare values
+typedef enum { LT=1111, EQ, GT } Comptype;
+
+// Struct for inputs to DECIDE. 
+typedef struct {
+    double LENGTH1;     // Lenth in LICs 0, 7, 12
+    double RADIUS1;     // Radius in LICs 1, 8, 13
+    double EPSILON;     // Deviation from PI in LICs 2, 9
+    double AREA1;       // Area in LICs 3, 10, 14
+    int Q_PTS;          // No. of consecutive points in LIC 4
+    int QUADS;          // No. of quadrants in LIC 4
+    double DIST;        // Distance in LIC 6
+    int N_PTS;          // No. of consecutive pts. in LIC 6
+    int K_PTS;          // No. of int. pts. in LICs 7, 12
+    int A_PTS;          // No. of int. pts. in LICs 8, 13
+    int B_PTS;          // No. of int. pts. in LICs 8, 13
+    int C_PTS;          // No. of int. pts. in LIC 9
+    int D_PTS;          // No. of int. pts. in LIC 9
+    int E_PTS;          // No. of int. pts. in LICs 10, 14
+    int F_PTS;          // No. of int. pts. in LICs 10, 14
+    int G_PTS;          // No. of int. pts. in LIC 11
+    double LENGTH2;     // Max length in LIC 12
+    double RADIUS2;     // Max radius in LIC 13
+    double AREA2;       // Max area in LIC 14
+} Parameters_t;
+
+// Input parameters
+static Parameters_t parameters;
+
+// Number of points
+static int numpoints;
+
+// X Coordinates of data points
+static double *x;
+
+// Y Coordinates of data points
+static double *y;
+
+// Logical Connector Matrix (LCM) 15*15
+static Connectors **lcm;
+
+// Preliminary Unlocking Matrix (PUM) 15*15
+static bool **pum;
+
+// Conditions Met Vector (CMV) 15
+static bool *cmv;
+
+// Final Unlocking Vector (FUV) 15
+static bool *fuv;
+
+// Launch Decision
+static bool launch;
+
+// Compare floating point numbers
+Comptype doubleCompare (double a, double b);
+
+// The goal DECIDE function. 
+void decide();
+
+#endif
