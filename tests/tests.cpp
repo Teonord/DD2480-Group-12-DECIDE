721c153a59db3454ce60ffaa41facdc38b0182a4
rifat4 <rifat_94@hotmail.com>
2025-01-25 19:32:13 +0100

    feat: implement LIC 0 from specs and test cases

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -22,6 +22,80 @@ TEST_CASE( "Rounded double comparison (doubleCompare)", "[doubleCompare]") {
     REQUIRE( doubleCompare(0.0000001, 0) == EQ);
 }
 
+// Tests for LIC 0
+
+TEST_CASE("negative distance", "[isConsecDistGTLen]") {
+    Parameters_t params;
+    params.NUMPOINTS = 2;
+    params.LENGTH1 = -1;
+    params.X = new double[2]{0, 1};
+    params.Y = new double[2]{0, 0};
+
+    REQUIRE(isConsecDistGTLen(params) == false);
+}
+
+TEST_CASE("too few points LIC 0", "[isConsecDistGTLen]") {
+    Parameters_t params;
+    params.NUMPOINTS = 1;
+    params.LENGTH1 = 1;
+    params.X = new double[1]{0};
+    params.Y = new double[1]{0};
+
+    REQUIRE(isConsecDistGTLen(params) == false);
+}
+
+TEST_CASE("2 points, dist longer than length1", "[isConsecDistGTLen]") {
+    Parameters_t params;
+    params.NUMPOINTS = 2;
+    params.LENGTH1 = 1;
+    params.X = new double[2]{0, 1};
+    params.Y = new double[2]{0, 1};
+
+    REQUIRE(isConsecDistGTLen(params) == true);
+}
+
+TEST_CASE("three points distances less than length1", "[isConsecDistGTLen]") {
+    Parameters_t params;
+    params.NUMPOINTS = 3;
+    params.LENGTH1 = 1;
+    params.X = new double[3]{0, 0.5, 1};
+    params.Y = new double[3]{0, 0.5, 1};
+
+    REQUIRE(isConsecDistGTLen(params) == false);
+}
+
+TEST_CASE("three points distances eq to length1", "[isConsecDistGTLen]") {
+    Parameters_t params;
+    params.NUMPOINTS = 3;
+    params.LENGTH1 = 1;
+    params.X = new double[3]{0, sqrt(1/2), sqrt(1/2) + sqrt(1/2)};
+    params.Y = new double[3]{0, sqrt(1/2), sqrt(1/2) + sqrt(1/2)};
+
+    REQUIRE(isConsecDistGTLen(params) == false);
+}
+
+TEST_CASE("almost within length", "[isConsecDistGTLen]") { //should be within length as within tolerance 10^-6
+    Parameters_t params;
+    params.NUMPOINTS = 3;
+    params.LENGTH1 = 1;
+    params.X = new double[3]{0, 0, 0};
+    params.Y = new double[3]{0, 1.000001, 2.000002};
+
+    REQUIRE(isConsecDistGTLen(params) == false);
+}
+
+TEST_CASE("point 4-5 outside of length", "[isConsecDistGTLen]") {
+    Parameters_t params;
+    params.NUMPOINTS = 5;
+    params.LENGTH1 = 1;
+    params.X = new double[5]{0, 0, 1, 2, 0};
+    params.Y = new double[5]{0, 1, 1, 1, 0};
+
+    REQUIRE(isConsecDistGTLen(params) == true);
+}
+
+
+
 // Tests for LIC 6
 
 TEST_CASE("too few points", "[isDistFromLine]") {
